7a8424b644634675b70594e0644f0c8204558fc2
Alexandr Kuzmich <92593750+xofmdo@users.noreply.github.com>
2022-11-02 11:56:16 +0300

    added models.UniqueConstraint in model and UniqueTogetherValidator in serializer, also added a default value for the owner field

--- cats/serializers.py
+++ cats/serializers.py
@@ -27,12 +27,20 @@ class CatSerializer(serializers.ModelSerializer):
     achievements = AchievementSerializer(many=True, required=False)
     color = serializers.ChoiceField(choices=CHOICES)
     age = serializers.SerializerMethodField()
+    owner = serializers.PrimaryKeyRelatedField(
+        read_only=True, default=serializers.CurrentUserDefault())
     
     class Meta:
         model = Cat
         fields = ('id', 'name', 'color', 'birth_year', 'achievements', 'owner',
                   'age')
         read_only_fields = ('owner',)
+        validators = [
+            UniqueTogetherValidator(
+                queryset=Cat.objects.all(),
+                fields=('name', 'owner')
+            )
+        ]
 
     def get_age(self, obj):
         return dt.datetime.now().year - obj.birth_year
